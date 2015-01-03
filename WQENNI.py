37e956f80d8a16190b5ef0a8b327a857bebfe91c
zhangzhaoyu <zhangzhaoyu0524@163.com>
2015-01-03 14:56:16 +0800

    WQENNI algorithm

---
+++ WQENNI.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+
+# author  zhangzhaoyu
+# Date    2015-01-03
+# Description:
+# WQENNI is depended on QENNI(quadrant encapsidated nearest based imputation)
+#
+#
+from numpy import *
+import matplotlib
+import matplotlib.pyplot as plt
+
